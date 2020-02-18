9f98d65ab76eeccc348c00ab5a1c81a127506707
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-02-18 23:14:28 +0100

    add yaml interpreter, resolve #4

--- src/main.py
+++ src/main.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
-
 import argparse
+import yaml
 from dataset import Dataset
-
-# TODO do config parsing
+from worker import Worker
 
 if __name__ == '__main__':
-    dataEnDe = Dataset('en-de', 'wmt19')
-    print('WMT19 en-de train:', len(dataEnDe.train.data))
-    print('WMT19 en-de dev:', len(dataEnDe.dev.data))
-    print('WMT19 en-de test:', len(dataEnDe.test.data))
-    print('WMT19 en-de blindtest:', len(dataEnDe.blind.data))
+    parser = argparse.ArgumentParser(description='MosQEto')
+    parser.add_argument('config', type=argparse.FileType('r'), help='YAML experiment config file')
+    args, unknown_args = parser.parse_known_args()
+    # TODO do config parsing
+
+    config = yaml.safe_load(args.config)
+    for arg in unknown_args:
+        sp = arg.split('=')
+        if len(sp) != 2:
+            raise Exception(f'Additional argument is not a key=value pair ({arg}).')
+        config[sp[0]] = sp[1]
+
+    if 'dataset' in config:
+        config['dataset'] = Dataset(config['dataset']['lang'], config['dataset']['org'])
+    
+    if 'method' not in config:
+        print(f'No `method` key in config, nothing to do.')
+        exit()
+    
+    plan = config.pop('method')
+    worker = Worker()
+
+    for planI in plan:
+        if hasattr(worker, planI):
+            func = getattr(worker, planI)
+            func(config)
+        else:
+            print(f'`{planI}` was not found on Worker.')
---
+++ src/worker/worker.py
@@ -0,0 +1,12 @@
+class Worker:
+    def train(self, options):
+        print("Doing some heavy training with a train dataset of size:", len(options['dataset'].train.data))
+        # TODO
+
+    def test(self, options):
+        print("Doing some positive testing on my test data")
+        # TODO
+
+    def inference(self, options):
+        print("Doing some inference on my blind data of size:", len(options['dataset'].blind.data))
+        # TODO
