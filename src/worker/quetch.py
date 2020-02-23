89890ce71fcc6c6d1193bad00349c32a860b520c
Ondřej Měkota <ondrej.mekota@me.com>
2020-02-23 13:47:20 +0100

    quetch sort of works

--- src/worker/quetch.py
+++ src/worker/quetch.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python3
 import kiwi
-import kiwibase as kb
+import worker.kiwibase as kb
 
 class Quetch:
     def train(self, options):
-        if options['use_kiwi']:
-            kiwi.train(options['quetch_config'])
         print("Doing some heavy training with a train dataset of size:", len(options['dataset'].train.data))
+        kiwi.train(options['quetch_train'])
 
     def test(self, options):
-        sentence = options['dataset'].train.data[0]
-        print(sentence)
+        raise NotImplementedError()
         print("Doing some positive testing on my test data")
 
     def inference(self, options):
-        kiwi_config = kb.load_kiwi_config(options['quetch_config'])
-        if options['use_kiwi']:
-            pred = kiwi.load_model(kiwi_config['load-model'])
-            # TODO finish this:
-            """
-            src = ['a b c', 'd e f g']
-            tgt = ['q w e r', 't y']
-            align = ['0-0 1-1 1-2', '1-1 3-0']
-            examples = [kiwi.constants.SOURCE: src,
-                        kiwi.constants.TARGET: tgt,
-                        kiwi.constants.ALIGNMENTS: align]
-            predictor.predict(examples)
-            """
-            pred.predict(examples)
+        """ Returns a list of predictions.
+        A list of lists, number for each word in sentence."""
+        data = options['dataset'].blind.data
+        print(options)
+
+        kiwi_config = kb.load_kiwi_config(options['quetch_test'])
+        pred = kiwi.load_model(kiwi_config['load-model'])
+        src = [' '.join(sent.src) for sent in data]
+        tgt = [' '.join(sent.tgt) for sent in data]
+        alg = [' '.join((f'{alg[0]}-{alg[1]}' for alg in sent.alignment)) for sent in data]
+        examples = {kiwi.constants.SOURCE: src, kiwi.constants.TARGET: tgt, kiwi.constants.ALIGNMENTS: alg}
 
         print("Doing some inference on my blind data of size:", len(options['dataset'].blind.data))
+        predictions = pred.predict(examples)
+        return predictions['tags']
