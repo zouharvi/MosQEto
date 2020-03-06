d0a54b25695d604ef0f7ba4914c794e3e8cfd316
Ondřej Měkota <ondrej.mekota@me.com>
2020-03-06 12:37:08 +0100

    transfer in progress

--- Makefile
+++ Makefile
@@ -6,12 +6,16 @@ create_wmt_pe_synth:
 	python3 src/main.py config/data/wmt19_pe_synth.yaml
 create_wmt_opus_random:
 	python3 src/main.py config/data/wmt19_opus_random.yaml
-create_opus:
+create_opus_random:
 	python3 src/main.py config/data/opus_random.yaml
 
 
-train:
+train_quetch:
 	python3 src/main.py config/quetch.yaml
+train_quetch_transfer:
+	# this also creates opus data, then trains, then created wmt data, and trains again
+	python3 src/main.py config/quetchtransfer.yaml
+
 validate:
 	python3 src/main.py config/validate.yaml
 
---
+++ src/worker/quetchtransfer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import kiwi
+import worker.kiwibase as kb
+
+class Quetch:
+    def train(self, options, call=[]):
+        print("Doing some heavy training with a train dataset of size:", len(options['dataset'].train.data))
+        kiwi.train(options['quetch_train'])
+
+    def inference(self, options, call=[]):
+        """ Returns a list of predictions.
+        A list of lists, number for each word in sentence."""
+        data = options['dataset'].blind.data
+
+        kiwi_config = kb.load_kiwi_config(options['quetch_test'])
+        pred = kiwi.load_model(kiwi_config['load-model'])
+        src = [' '.join(sent.src) for sent in data]
+        tgt = [' '.join(sent.tgt) for sent in data]
+        alg = [' '.join((f'{alg[0]}-{alg[1]}' for alg in sent.alignment)) for sent in data]
+        examples = {kiwi.constants.SOURCE: src, kiwi.constants.TARGET: tgt, kiwi.constants.ALIGNMENTS: alg}
+
+        print("Doing some inference on my blind data of size:", len(options['dataset'].blind.data))
+        predictions = pred.predict(examples)
+        return predictions['tags']
